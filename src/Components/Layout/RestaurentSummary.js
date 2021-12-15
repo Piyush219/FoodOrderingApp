54dacbfdda5e18a3d8ec38e3654747346ca38295
Piyush219 <piyush66642k@gmail.com>
2021-12-15 19:33:32 +0530

    Restaurent Summary Block

---
+++ src/Components/Layout/RestaurentSummary.js
@@ -0,0 +1,23 @@
+import styles from './RestaurentSummary.module.css'
+
+const RestaurentSummary = () =>{
+    return (
+        
+        <div className= {styles.summaryBody}>
+            <h1>
+                Delicious Food, Delivered To You
+            </h1>
+
+            <p>
+                Choose your favourite meal from our broad selection of available
+                meals and enjoy a delicious lunch or dinner at home.
+                <br />
+                All our meals are cooked with high-quality ingredients, just-in-time
+                and of course by experienced chefs!
+            </p>
+        </div>
+        
+    )
+}
+
+export default RestaurentSummary;
